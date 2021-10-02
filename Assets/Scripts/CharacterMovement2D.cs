3fb82608b29337eecdd2d7de9b7cb26a3ca7510f
Steve <steverobray@gmail.com>
2021-10-02 13:43:39 +0100

    Basic character movement and control

--- Assets/Scripts/CharacterMovement2D.cs
+++ Assets/Scripts/CharacterMovement2D.cs
@@ -2,14 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CharacterMovement2D : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1F;
     [SerializeField] private float jumpForce = 1F;
-
+    
+    [SerializeField] private LayerMask groundLayer = default;
+    [SerializeField] private Transform groundPoint = default;
+    [SerializeField] private Transform leftPoint = default;
+    [SerializeField] private Transform rightPoint = default;
+    
+    [SerializeField] private UnityEvent changeDirectionEvent = default;
+    [SerializeField] private UnityEvent jumpEvent = default;
+    
+    private bool _isGrounded = true;
+    private bool _direction = true; // false left, true right
+    private float _move = 0;
+    
     private Rigidbody2D _rigidbody = default;
     
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -18,17 +32,48 @@ public class CharacterMovement2D : MonoBehaviour
     private void Update()
     {
         // Update Movement
-        var move = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(move, 0, 0) * (Time.deltaTime * moveSpeed);
+        _move = Input.GetAxis("Horizontal");
+        _rigidbody.velocity = new Vector2(moveSpeed * _move, _rigidbody.velocity.y);
         
-        // Ground check
-        //if(!Mathf.Approximately(0, move))
-        
+        CheckDirection();
+        SlideDownWalls();
+
         // Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            _rigidbody.AddForce(new Vector2(0, jumpForce));
+            jumpEvent.Invoke();
+            _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
-        
     }
+
+    private bool IsGrounded()
+    {
+        var colliders = Physics2D.OverlapCircleAll(groundPoint.position, .05f, groundLayer);
+        _isGrounded = colliders.Length > 0;
+        return _isGrounded;
+    }
+
+    private void CheckDirection()
+    {
+        var newDirection = _move > 0;
+        
+        if (_move == 0) return;
+        if (_direction == newDirection) return;
+        
+        _direction = newDirection;
+        changeDirectionEvent.Invoke();
+    }
+
+    private void SlideDownWalls()
+    {
+        // get collisions
+        var left = Physics2D.OverlapCircleAll(leftPoint.position, .05f, groundLayer).Length;
+        var right = Physics2D.OverlapCircleAll(rightPoint.position, .05f, groundLayer).Length;
+
+        if (left > 0 && !_direction || right > 0 && _direction)
+        {
+            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+        }
+    }
+    
 }
